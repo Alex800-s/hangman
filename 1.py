69987382f68bc5574d08b305aae7324b0b54a48d
Alex800-s <lin191@yandex.ru>
2024-12-15 19:04:46 +0300

    Alex800-s

---
+++ 1.py
@@ -0,0 +1,10 @@
+class Square():
+    square_list=[]
+    def __init__(self,x):
+        self.x=x
+        self.square_list.append(self.x)
+qwe=Square(12)
+ewr=Square(33)
+www=Square(56)
+print(qwe.square_list)
+
